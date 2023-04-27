9dceb35cf6c8d3f383da4e085704afb206d3967e
ZorianaKuzyk <zoryanakyzuk9@gmail.com>
2023-04-27 16:49:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import profileImage from "./img/1519855918965.jfif";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My name</h1>
+      <h2>My dog</h2>
+      <img src={profileImage} alt="profile" />
+    </div>
+  );
+}
+
+export default App;
